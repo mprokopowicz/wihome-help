3006ef8f91160e2f926451e401c8d4577336ca69
Michał <michal.prokopowicz@homersoft.com>
2013-05-18 04:25:16 +0200

    fixed routing

--- js/app.js
+++ js/app.js
@@ -1,13 +1,12 @@
 'use strict';
 
-angular.module('WihomeHelp', 
-	['Centralway.lungo-angular-bridge', 
-	 'BridgeExample.filters',
-	 'WihomeHelp.services', 
-	 'BridgeExample.directives'])
- 
-	.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
-        $routeProvider.when('help/view/:id', { templateUrl: '/partials/help.html', controller: 'HelpController'});
-		$routeProvider.otherwise({redirectTo: '/'});
-		$locationProvider.html5Mode(true);
-  }]);
+angular.module('WihomeHelp',
+    ['Centralway.lungo-angular-bridge',
+        'BridgeExample.filters',
+        'WihomeHelp.services',
+        'BridgeExample.directives'])
+
+    .config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+        $routeProvider.when('/help/view/:id', {templateUrl: '/partials/help.html', controller: 'HelpController'});
+        $routeProvider.otherwise({redirectTo: '/'});
+    }]);
