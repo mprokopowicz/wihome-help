a486ebaac056b67ba94b95030992503faa862e9a
Michał Prokopowicz <michal.prokopowicz@homesoft.com>
2013-05-20 10:06:55 +0200

    debugging stuff

--- js/controllers.js
+++ js/controllers.js
@@ -1,33 +1,40 @@
 'use strict';
 
 function toggleRightAside() {
-  Lungo.View.Aside.toggle('#rightaside');
+	Lungo.View.Aside.toggle('#rightaside');
 }
 
 function AppCtrl($scope) {
-    $scope.name = "Some name";
-    /**
-	  * A short cut to manually refresh the application from the example/ directory
-	  */
-    $scope.refreshApplication = function() {
-    	window.location = '/examples/simple/index.html';
-    }
-    console.log('AppCtrl::AppCtrl() - Instantiated');
-    
-    $scope.triggerAside = function() {
-        console.log('triggering aside');
-        Lungo.Router.aside('main', 'aside1');
-    }
+	$scope.name = "Some name";
+	/**
+	 * A short cut to manually refresh the application from the example/ directory
+	 */
+	$scope.refreshApplication = function() {
+		window.location = '/examples/simple/index.html';
+	}
 
-    $scope.triggerRightAside = toggleRightAside;
+
+	scope.$on('$routeChangeStart', function(next, current) {
+		console.log(arguments);
+	});
+
+
+	console.log('AppCtrl::AppCtrl() - Instantiated');
+
+	$scope.triggerAside = function() {
+		console.log('triggering aside');
+		Lungo.Router.aside('main', 'aside1');
+	}
+
+	$scope.triggerRightAside = toggleRightAside;
 }
 
 function SearchController($scope, LibraryIndex) {
 	$scope.query = "";
 	$scope.results = [];
-	
-	$scope.$watch('query', function(){
-		if($scope.query.length > 0) {
+
+	$scope.$watch('query', function() {
+		if ($scope.query.length > 0) {
 			$scope.results = LibraryIndex.searchByName($scope.query);
 		} else {
 			$scope.results = [];
@@ -36,14 +43,14 @@ function SearchController($scope, LibraryIndex) {
 }
 
 function IndexAsideController($scope, LibraryIndex) {
-	
+
 	var index = LibraryIndex.getIndex();
 	$scope.libraryIndex = index;
 	$scope.title = "Library";
 	$scope.isRoot = true;
-	
+
 	$scope.setCategories = function(category) {
-		if( category.subcategories.length ) {
+		if (category.subcategories.length) {
 			$scope.isRoot = false;
 			$scope.title = category.name;
 			$scope.libraryIndex = category.subcategories;
@@ -51,26 +58,26 @@ function IndexAsideController($scope, LibraryIndex) {
 			return;
 		}
 	};
-	
-	$scope.goBack = function(){
+
+	$scope.goBack = function() {
 		$scope.libraryIndex = index;
 		$scope.isRoot = true;
 		$scope.title = "Library";
 	};
-	
+
 }
 
 function MainController($scope) {
-  $scope.callTopLevel = function() {
-    alert('Top level called!');
-  };
+	$scope.callTopLevel = function() {
+		alert('Top level called!');
+	};
 }
 
 function HelpController($scope, $routeParams, LibraryIndex) {
-  
-	$scope.$on('$routeChangeSuccess', function(){
+
+	$scope.$on('$routeChangeSuccess', function() {
 		$scope.idHelp = $routeParams.id;
-		$scope.help = LibraryIndex.getById( $routeParams.id );
+		$scope.help = LibraryIndex.getById($routeParams.id);
 	});
-	
+
 }
